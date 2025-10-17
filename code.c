a9d7731e51619ae0da050e6281675dab0619033b
NotPro955 <bharathvarma955@gmail.com>
2025-10-17 12:42:50 +0530

    Adding Conditions for Password

--- code.c
+++ code.c
@@ -2,5 +2,13 @@
 
 int main(){
 
+    printf("Conditions to be followed: \n");
+    printf("1.Password should have atleast 8 characters\n");
+    printf("2.Password should have aleast 1 digit\n");
+    printf("3.Password should have atleast 1 special character\n");
+
+    char password[100];
+    printf("Enter your password: ");
+    scanf("%c", &password);
     return 0;
 }
